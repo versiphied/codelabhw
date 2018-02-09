369be43822cf1f53aea6ede4e661a7959e7f143a
Unknown <jjb672@nyu.edu>
2018-02-08 22:21:30 -0500

    Added CodeLab Unity Project

---
+++ Code Lab Hw 1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveSpeed = 0.1f;
+
+    public KeyCode D = KeyCode.D;
+    public KeyCode A = KeyCode.A;
+
+    public float bottomOfScreen = -5f;
+
+    public string sceneToSwitchToOnDeath = "GameOverScene";
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(D))
+        {
+            transform.Translate(moveSpeed, 0, 0);
+        }
+        if (Input.GetKey(A))
+        {
+            transform.Translate(-moveSpeed, 0, 0);
+        }
+
+        if (transform.position.y < bottomOfScreen)
+        {
+            SceneManager.LoadScene(sceneToSwitchToOnDeath);
+        }
+    }
+}
---
+++ Code Lab Hw 1/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;  
+
+	public KeyCode rightKey = KeyCode.RightArrow;
+	public KeyCode leftKey = KeyCode.LeftArrow;
+    public KeyCode upkey = KeyCode.UpArrow;
+	public float bottomOfScreen = -5f;
+
+	public string sceneToSwitchToOnDeath = "GameOverScene";
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+   // rb = GetComponent<Rigidbody2D>();
+     //        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(upkey) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(rightKey)) {
+			transform.Translate(moveSpeed, 0, 0);
+		}
+		if (Input.GetKey(leftKey)) {
+			transform.Translate(-moveSpeed, 0, 0);
+		}
+
+		if (transform.position.y < bottomOfScreen) {
+			SceneManager.LoadScene(sceneToSwitchToOnDeath);
+		}
+	}
+}
